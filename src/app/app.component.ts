cb3507d523fdc78f11f7b14063ed221a5d5b54ce
nikhilyam <nik.yamsalwar@gmail.com>
2022-03-07 09:28:48 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,1 @@
-import { Component, VERSION } from '@angular/core';
-
-@Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
-})
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-}
+import { Component, Injector } from '@angular/core';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { PopupComponent } from './popup.component';
+import { PopupService } from './popup.service';
+
+@NgModule({
+  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [PopupComponent],
+  providers: [PopupService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
